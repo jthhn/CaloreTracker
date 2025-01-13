38a57f30a967f5149623d256038ec2cd23915e10
jthhn <jithinam530@gmail.com>
2025-01-13 16:53:23 +0530

    version 1

--- CalTracker/tracker/urls.py
+++ CalTracker/tracker/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'tracker'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('delete/<int:id>/', views.delete_consume, name='delete'),
 ]
